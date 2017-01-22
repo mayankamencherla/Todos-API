cfc7d266c78144b099d8004c9f12bc813aff0019
mayankamencherla <mayank.amencherla@razorpay.com>
2017-01-22 15:44:59 +0530

    Added route GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -5,12 +5,14 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {ObjectID} = require('mongodb');
 
 var app = express();
 
 app.use(bodyParser.json()); // middleware for express
 
 /*----------------------- All routes start----------------------------------*/
+// Create a new todo and save
 app.post('/todos', (req, res) => {
 
     var todo = new Todo({
@@ -24,12 +26,29 @@ app.post('/todos', (req, res) => {
     });
 });
 
-
+// Get all todos
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos}); // returning a todos object
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+// Get one todo by id
+// req params contains the params passed into the request
+app.get('/todos/:id', (req, res) => {
+
+    var id = req.params.id; // getting the id from the dynamic url
+
+    if (!ObjectID.isValid(id)){
+        res.send(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        res.send(todo);
+    }, (e) => {
+        res.send(400).send(e);
     });
 });
 
