4a1ae9ed1daabf0420c6d6bd780a9854893baa24
mayankamencherla <mayank.amencherla@razorpay.com>
2017-01-22 19:16:21 +0530

    Added a route to delete a todo

--- server/server.js
+++ server/server.js
@@ -44,17 +44,37 @@ app.get('/todos/:id', (req, res) => {
     var id = req.params.id; 
 
     if (!ObjectID.isValid(id)){
-        return res.send(404).send();
+        return res.status(404).send();
     }
 
     Todo.findById(id).then((todo) => {
         if (!todo){
-            return res.send(404).send();
+            return res.status(404).send();
         }
         
         res.send({todo});
     }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo){
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }, (e) => {
         res.send(400).send(e);
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
